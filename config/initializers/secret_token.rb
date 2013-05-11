38269bb16c995f9d14431cbe790966ee4fa77cd3
samuel.nasuti <samuel.nasuti@gmail.com>
2013-05-11 18:02:43 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SwMap::Application.config.secret_token = '7bfa69869883cecdf44366521acb33d830f52b931706a09ae3516c1a0cab7c914397b062d9984eacd345c8678a525aa8656cfa595d4696267c7a84af85007c77'
